83546b1402999aab9b9e4940149b3d4f4324567f
Paulo Roberto <robsilveirajunior@gmail.com>
2022-03-29 14:16:22 -0300

    layout

--- src/components/products/ProductList.tsx
+++ src/components/products/ProductList.tsx
@@ -19,21 +19,21 @@ export const ProductListContainer: React.FC = () => {
       <ProductBase
         imageUrl="https://dkdkf5wiwtstx.cloudfront.net/Custom/Content/Products/10/89/1089616_blusa-cropped-chenile-trama-100277816_m2_637822572985577933.jpg"
         listPrice={119}
-        salePrice={119}
+        salePrice={68}
         name="blusão off-white"
         id={1}
       />
       <ProductBase
         imageUrl="https://d2akm0nhugecrh.cloudfront.net/Custom/Content/Products/10/07/1007061_calca-jogger-ca-lippa-443110047_m70_637819138811696686.jpg"
         listPrice={159}
-        salePrice={119}
+        salePrice={42}
         name="calça jogger cintura alta em sarja"
         id={2}
       />
       <ProductBase
         imageUrl="https://dkdkf5wiwtstx.cloudfront.net/Custom/Content/Products/10/85/1085353_calca-jeans-wideleg-cropped-ca-analu-100201707_m4_637794950956042294.jpg"
         listPrice={119}
-        salePrice={119}
+        salePrice={89}
         name="calça jeans wide leg cropped"
         id={3}
       />

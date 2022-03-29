462c4046196373fb959ec2b10bcf1d1968c397b8
Paulo Roberto <robsilveirajunior@gmail.com>
2022-03-29 12:45:05 -0300

    fix

--- src/components/products/productFavorite.tsx
+++ src/components/products/productFavorite.tsx
@@ -6,6 +6,7 @@ export interface ProductFavoriteProps {
   isFavorite: boolean
   onFavoriteClicked: () => void
 }
+
 const FavoriteButton = styled.button<{ isFavorite: boolean }>`
   background: ${props => (props.isFavorite ? 'red' : 'white')};
   color: ${props => (props.isFavorite ? 'white' : 'black')};
@@ -21,13 +22,19 @@ const FavoriteButton = styled.button<{ isFavorite: boolean }>`
 `
 export const ProductFavorite: React.FC<ProductFavoriteProps> = props => {
   const [isFavorite, setIsFavorite] = useState(props.isFavorite)
+
+  // Check if the product ID is in localStorage
+  const isFavoriteID = (id: number): boolean => {
+    const favoriteIds = JSON.parse(localStorage.getItem('favoriteIds') || '[]')
+    return favoriteIds.includes(id)
+  }
   const handleFavoriteClicked = useCallback(() => {
     setIsFavorite(!isFavorite)
     props.onFavoriteClicked()
   }, [isFavorite, props.onFavoriteClicked])
   return (
     <FavoriteButton isFavorite={isFavorite} onClick={handleFavoriteClicked}>
-      {isFavorite ? 'Remover dos favoritos' : 'Adicionar aos favoritos'}
+      {isFavoriteID ? 'Remover dos favoritos' : 'Adicionar aos favoritos'}
     </FavoriteButton>
   )
 }
